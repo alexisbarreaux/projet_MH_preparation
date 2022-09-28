dbaaaa162724f6d57b4ba2ae56fbc60eb7431ef8
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-09-28 12:24:45 +0200

    Split first test file

--- binpacking/utils/file_splitting_utils.py
+++ binpacking/utils/file_splitting_utils.py
@@ -1,6 +1,11 @@
-from utils import ROOT_PATH
+from typing import Tuple, List
 
-DIR_PATH = ROOT_PATH / "binpacking"
+from utils import ROOT_DIR
+
+BINPACKING_DIR = ROOT_DIR / "binpacking"
+TEST_FILES_DIR = BINPACKING_DIR / "test_files"
+BASE_TEST_FILES_DIR = TEST_FILES_DIR / "base_files"
+SPLIT_TEST_FILES_DIR = TEST_FILES_DIR / "split_files"
 
 
 def split_test_file(source_file_path: str) -> None:
@@ -13,19 +18,66 @@ def split_test_file(source_file_path: str) -> None:
         number_of_subproblems = int(source_file.readline())
 
         # For each get its parameters
-        for i in range(number_of_subproblems):
-            identifier = source_file.readline().strip()
-            problems_parameters = source_file.readline().strip().split(" ")
-            [bin_capacity, number_of_items, optimal_bin_numbers] = list(
-                map(int, problems_parameters)
-            )
-            # Get the items
-            for items in range(number_of_items):
-                item_size = int(source_file.readline())
-                print(item_size)
+        for _ in range(number_of_subproblems):
+            write_test_file_from_parameters(*read_single_problem_from_file(source_file))
+    return
 
+
+def read_single_problem_from_path(
+    source_file_path: str,
+) -> Tuple[str, int, int, int, List[int]]:
+    """
+    Used to read a file containing a unique problem
+    or the next problem of a file when file path is given.
+    """
+    with open(source_file_path, "r") as source_file:
+        return read_single_problem_from_file(source_file)
+
+
+def read_single_problem_from_file(
+    source_file,
+) -> Tuple[str, int, int, int, List[int]]:
+    """
+    Used to read a file containing a unique problem
+    or the next problem of a file when file is given.
+    """
+    identifier = source_file.readline().strip()
+    problems_parameters = source_file.readline().strip().split(" ")
+    [bin_capacity, number_of_items, optimal_bin_numbers] = list(
+        map(int, problems_parameters)
+    )
+    # Get the items
+    items = [int(source_file.readline()) for j in range(number_of_items)]
+
+    return (
+        identifier,
+        bin_capacity,
+        number_of_items,
+        optimal_bin_numbers,
+        items,
+    )
+
+
+def write_test_file_from_parameters(
+    identifier: str,
+    bin_capacity: int,
+    number_of_items: int,
+    optimal_bin_numbers: int,
+    items: List[int],
+    destination_path: str = SPLIT_TEST_FILES_DIR,
+) -> None:
+    """
+    Creates a test file from the parameters of a problem.
+    """
+    with open(destination_path / (identifier + ".txt"), "w") as destination_file:
+        destination_file.write(identifier)
+        destination_file.write(
+            "\n" + f"{bin_capacity} {number_of_items} {optimal_bin_numbers}"
+        )
+        for i in range(number_of_items):
+            destination_file.write("\n" + str(items[i]))
     return
 
 
 if __name__ == "__main__":
-    split_test_file(DIR_PATH / "test_files/binpack1.txt")
+    split_test_file(BASE_TEST_FILES_DIR / "binpack1.txt")
--- utils/pathes.py
+++ utils/pathes.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-ROOT_PATH = Path.cwd()
+ROOT_DIR = Path.cwd()
